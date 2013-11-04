bfb617eaab630bb1b2864077e2313fca47cd4b5e
Vinney Cavallo <vcavallo@gmail.com>
2013-11-04 13:56:32 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PollywogRanchRailsRuby003::Application.config.secret_key_base = 'd9a901ccbbcebf493729aa86846529610965cdbde79e9fc8d9cfc86f098087303021957b2cc00af7ee3ef8fdcc8c6b1bbbfbd6abee8d0eb624c2efbc4b8826f9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PollywogRanchRailsRuby003::Application.config.session_store :cookie_store, key: '_pollywog-ranch-rails-ruby-003_session'
